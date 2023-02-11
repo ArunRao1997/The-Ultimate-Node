dbcbfdbfad88a253afc365ffececf9b96f40443f
Your Name <you@example.com>
2023-02-11 16:52:30 -0500

    Node Learning

---
+++ test.js
@@ -0,0 +1,16 @@
+const calculator = require("./calculator")
+
+console.log('Hello')
+
+function sayHello(){
+    console.log("Hello from Arun")
+}
+
+sayHello()
+
+calculator.addition(3, 4)
+calculator.subtraction(5, 4)
+calculator.multiplication(3, 4)
+calculator.division(10, 3)
+
+//console.log(global)
